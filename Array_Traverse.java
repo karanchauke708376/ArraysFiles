a07ec1c342f2d9370686c0f4b8cd4b56fa3c5b7a
Karan Chauke <69950796+karanchauke708376@users.noreply.github.com>
2022-04-30 20:47:45 +0530

    Initial Commit On The First File Add

---
+++ Array_Traverse.java
@@ -0,0 +1,32 @@
+package Arrays_Practice_JavaTPoint;
+
+public class Array_Traverse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// Declaration Of Instantiation
+		int arr[] = new int[10];
+		
+		arr[0] = 45;
+		arr[1] = 78;
+		arr[2] = 14;
+		arr[3] = 64;
+		arr[4] = 10;
+		arr[5] = 33;
+		arr[6] = 91;
+		arr[7] = 37;
+		arr[9] = 47;
+		
+		// Traversing array ;
+		
+		for(int i = 0; i < arr.length; i++) {
+			
+			System.out.print(arr[i]+" ");
+		}
+		
+		
+		
+	}
+
+}
