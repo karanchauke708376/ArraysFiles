60c22441ceeb8855a276bb182d17d5032c84ac03
Karan Chauke <69950796+karanchauke708376@users.noreply.github.com>
2022-04-30 20:53:09 +0530

    Initial Commit On Add Array_Traverse_I.java Files

---
+++ Array_Traverse_I.java
@@ -0,0 +1,16 @@
+package Arrays_Practice_JavaTPoint;
+
+public class Array_Traverse_I {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a[] = {78, 64, 31, 04, 97, 37, 64, 15, 59, 11, 9};
+		
+		for(int i = 0; i < a.length; i++) {
+			
+			System.out.print(a[i]+" ");
+		}
+	}
+
+}
